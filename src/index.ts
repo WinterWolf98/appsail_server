11a83b35ebcba1f5cdc21d4d82f2867474487c2a
WinterWolf98 <67791595+WinterWolf98@users.noreply.github.com>
2024-09-16 15:16:35 +0530

    updated auth

--- src/index.ts
+++ src/index.ts
@@ -14,18 +14,19 @@ app.use(cors({ origin: true })); // add cors headers
 process.env.DEBUG === 'true' && app.use(morgan('dev')); // add logging
 app.use(async (req, res, next) => {
   const app = Catalyst.initialize(req as unknown as Record<string, unknown>);
-  // const currentUser = await app.userManagement().getCurrentUser();
-  // if(!currentUser) {
-  //   return res.redirect('/__catalyst/auth/login');
-  // }
-  if(req.url === '/') {
-    return res.redirect('/app');
+  const currentUser = await app.userManagement().getCurrentUser().catch((er) => console.log("user error: ", er));
+  if(!currentUser) {
+    return res.redirect('/__catalyst/auth/login');
   }
   res.locals.app = app;
   next();
 });
 
-app.use('/app/', Express.static('./client'));
+app.get('/', (req, res) => {
+  res.redirect('/app');
+});
+
+app.use('/app', Express.static('./client'));
 
 app.use('/apis', (req, res) => {
   return res.status(200).send('success');
